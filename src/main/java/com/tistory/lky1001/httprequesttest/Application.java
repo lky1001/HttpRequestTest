2869615497010a983847a5d90b394950f72e1999
kylee <kylee@devband.com>
2016-10-17 09:10:15 +0900

    CRLF를 이용하 GET요청 헤더 출력하기. 메시지 끝 구분하기

--- src/main/java/com/tistory/lky1001/httprequesttest/Application.java
+++ src/main/java/com/tistory/lky1001/httprequesttest/Application.java
@@ -11,6 +11,8 @@ import java.net.Socket;
  */
 public class Application {
 
+    private static final byte CR = '\r';
+    private static final byte LF = '\n';
     public static void main(String[] args) throws IOException {
         Application application = new Application();
 
@@ -26,9 +28,29 @@ public class Application {
         OutputStream out = socket.getOutputStream();
 
         int oneInt = -1;
+        byte old = (byte) -1;
+
+        StringBuilder sb = new StringBuilder();
+        int lineNumber = 0;
 
         while(-1 != (oneInt = in.read())) {
-            System.out.print((char) oneInt);
+            byte data = (byte) oneInt;
+            // CRLF 찾기, 이전 CRLF다음부터 여기까지가 1행이 된다.
+            if (data == Application.LF && old == Application.CR) {
+                String line = sb.substring(0, sb.length() - 1);
+                lineNumber++;
+                System.out.printf("%d: %s\n", lineNumber, line);
+
+                // 행의 내용이 없음. 즉 헤더의 끝
+                if (line.length() <= 0) {
+                    System.out.println("헤더 끝");
+                    break;
+                }
+                sb.setLength(0);
+            } else {
+                sb.append((char)data);
+            }
+            old = (byte) oneInt;
         }
 
         out.close();
