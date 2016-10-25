b213b6922fc8c48060678b1cc3ed9e4813d7b423
kylee <kylee@devband.com>
2016-10-25 09:29:27 +0900

    http GET, POST 요청 파라미터 파싱하기

--- README.md
+++ README.md
@@ -2,8 +2,37 @@
 
 ### http 리퀘스트 데이터 확인하기 ###
 
-1. [HTTP 요청 출력해보기](https://github.com/lky1001/HttpRequestTest/tree/print_http_request)
-2. [CRLF로 GET 요청 헤더 행 구분하여 출력하기](https://github.com/lky1001/HttpRequestTest/tree/handle_get_request)
-3. [메시지 바디 읽기](https://github.com/lky1001/HttpRequestTest/tree/handle_body_message)
+1. CRLF를 이용해 헤더의 행 구분하기
+2. 헤더 값 읽기
+3. GET 요청이 아니면 메시지 바디 읽기
+4. GET 요청이면 url에 파라미터 읽기
+5. POST 요청이면 body에 파라미터 읽기
+4. 헤더 출력
+5. 파라미터 출력
 
-- 레퍼런스 : [웹 프로그래머를 위한 서블릿 컨테이너의 이해](http://book.naver.com/bookdb/book_detail.nhn?bid=7082377)
+### 사용법 ###
+- 자바 프로그램 실행
+- GET 요청 시 브라우저에서 http://localhost:8000?aaa=8&bbb=9 요청
+- 자바 콘솔 확인
+- curl로 POST 요청 시 : curl -XPOST 'http://localhost:8000' -d 'aaa=8&bbb=9'
+- curl로 GET 요청 시 : curl -XPOST 'http://localhost:8000?aaa=8&bbb=9'
+
+```
+/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/bin/java -agentlib:jdwp=transport=dt_socket,address=127.0.0.1:56467,suspend=y,server=n -javaagent:/Users/lee/Library/Caches/IntelliJIdea15/groovyHotSwap/gragent.jar -Dfile.encoding=UTF-8 -classpath "/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/ant-javafx.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/dt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/javafx-mx.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/jconsole.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/sa-jdi.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/lib/tools.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/charsets.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/deploy.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/htmlconverter.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/javaws.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/jce.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/jfr.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/jfxswt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/jsse.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/management-agent.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/plugin.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/resources.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/rt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/cldrdata.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/dnsns.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/jfxrt.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/localedata.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/nashorn.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/sunec.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/sunjce_provider.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/sunpkcs11.jar:/Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/ext/zipfs.jar:/Users/lee/Documents/workspace/HttpRequestTest/build/classes/main:/Users/lee/Documents/workspace/HttpRequestTest/build/resources/main:/Applications/IntelliJ IDEA 15.app/Contents/lib/idea_rt.jar" com.tistory.lky1001.httprequesttest.Application
+objc[5874]: Class JavaLaunchHelper is implemented in both /Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/bin/java and /Library/Java/JavaVirtualMachines/jdk1.8.0_11.jdk/Contents/Home/jre/lib/libinstrument.dylib. One of the two will be used. Which one is undefined.
+Connected to the target VM, address: '127.0.0.1:56467', transport: 'socket'
+METHOD : POST REQ : / HTTP VER. HTTP/1.1
+Header list
+ Key : Accept Value :  */*
+ Key : User-Agent Value :  curl/7.43.0
+ Key : Host Value :  localhost:8000
+ Key : Content-Length Value :  11
+ Key : Content-Type Value :  application/x-www-form-urlencoded
+paramName: aaa paramValue: 8
+paramName: bbb paramValue: 9
+End of HTTP Message.
+Disconnected from the target VM, address: '127.0.0.1:56467', transport: 'socket'
+
+Process finished with exit code 0
+
+```
--- src/main/java/com/tistory/lky1001/httprequesttest/Application.java
+++ src/main/java/com/tistory/lky1001/httprequesttest/Application.java
@@ -5,11 +5,16 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.util.*;
 
 /**
  * Created by lee on 2016. 10. 12..
  */
 public class Application {
+
+    private static final byte CR = '\r';
+    private static final byte LF = '\n';
 
     public static void main(String[] args) throws IOException {
         Application application = new Application();
@@ -23,16 +28,137 @@ public class Application {
         serverSocket = new ServerSocket(8000);
         Socket socket = serverSocket.accept();
         InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
 
         int oneInt = -1;
+        byte old = (byte) -1;
 
-        while(-1 != (oneInt = in.read())) {
-            System.out.print((char) oneInt);
+        StringBuilder sb = new StringBuilder();
+
+        int lineNumber = 0;
+        boolean bodyFlag = false;
+        String method = null;
+        String requestUrl = null;
+        String httpVersion = null;
+        int contentLength = -1;
+        int bodyRead = 0;
+
+        List<Byte> bodyByteList = null;
+        Map<String, String> headerMap = new HashMap<>();
+
+        while (-1 != (oneInt = in.read())) {
+            byte data = (byte)oneInt;
+
+            if (bodyFlag) {
+                bodyRead++;
+                bodyByteList.add(data);
+                if (bodyRead >= contentLength) {
+                    break;
+                }
+            } else {
+                if (data == Application.LF && old == Application.CR) {
+                    String oneLine = sb.substring(0, sb.length() - 1);
+                    lineNumber++;
+                    if (lineNumber == 1) {
+                        int firstBlank = oneLine.indexOf(" ");
+                        int secondBlank = oneLine.lastIndexOf(" ");
+                        method = oneLine.substring(0, firstBlank);
+                        requestUrl = oneLine.substring(firstBlank + 1, secondBlank);
+                        httpVersion = oneLine.substring(secondBlank + 1);
+                    } else {
+                        if (oneLine.length() <= 0) {
+                            bodyFlag = true;
+                            // 헤더 끝, GET 요청이면 바디가 없음
+                            if ("GET".equals(method)) {
+                                break;
+                            }
+                            String contentLengthValue = headerMap.get("Content-Length");
+                            if (contentLengthValue != null) {
+                                contentLength = Integer.parseInt(contentLengthValue.trim());
+                                bodyFlag = true;
+                                bodyByteList = new ArrayList<>();
+                            }
+                            continue;
+                        }
+                        int indexOfColon = oneLine.indexOf(":");
+                        String headerName = oneLine.substring(0, indexOfColon);
+                        String headerValue = oneLine.substring(indexOfColon + 1);
+                        headerMap.put(headerName, headerValue);
+                    }
+                    sb.setLength(0);
+                } else {
+                    sb.append((char) data);
+                }
+            }
+            old = (byte) oneInt;
         }
 
-        out.close();
         in.close();
         socket.close();
+
+        System.out.printf("METHOD : %s REQ : %s HTTP VER. %s\n", method, requestUrl, httpVersion);
+        Map<String, String> paramMap = new HashMap<>();
+        // url에 쿼리스트링이 있는지 검사
+        int indexOfQuotation = requestUrl.indexOf("?");
+        if (indexOfQuotation > 0) {
+            // '?' 다음 문자열을 '&'로 자르기
+            StringTokenizer st = new StringTokenizer(requestUrl.substring(indexOfQuotation + 1), "&");
+            while (st.hasMoreTokens()) {
+                String params = st.nextToken();
+                paramMap.put(params.substring(0, params.indexOf("=")),
+                    params.substring(params.indexOf("=") + 1));
+            }
+        }
+        System.out.println("Header list");
+        Set<String> keySet = headerMap.keySet();
+        Iterator<String> keyIter = keySet.iterator();
+        while (keyIter.hasNext()) {
+            String headerName = keyIter.next();
+            System.out.printf(" Key : %s Value : %s\n", headerName, headerMap.get(headerName));
+        }
+
+        if (bodyByteList != null) {
+            if ("application/x-www-form-urlencoded".equals(headerMap.get("Content-Type").trim())) {
+                int startIndex = 0;
+                byte[] srcBytes = new byte[bodyByteList.size()];
+                String currentName = null;
+
+                for (int i = 0; i < bodyByteList.size(); i++) {
+                    byte oneByte = bodyByteList.get(i);
+                    srcBytes[i] = oneByte;
+                    if ('=' == oneByte) {
+                        byte[] one = new byte[i - startIndex];
+                        System.arraycopy(srcBytes, startIndex, one, 0, i - startIndex);
+                        currentName = URLDecoder.decode(new String(one), "CP949");
+                        startIndex = i + 1;
+                    } else if ('&' == oneByte) {
+                        byte[] one = new byte[i - startIndex];
+                        System.arraycopy(srcBytes, startIndex, one, 0, i - startIndex);
+                        paramMap.put(currentName, URLDecoder.decode(new String(one), "CP949"));
+                        startIndex = i + 1;
+                    } else if (i == bodyByteList.size() - 1) {
+                        byte[] one = new byte[i - startIndex + 1];
+                        System.arraycopy(srcBytes, startIndex, one, 0, i - startIndex + 1);
+                        paramMap.put(currentName, URLDecoder.decode(new String(one), "CP949"));
+                        startIndex = i + 1;
+                    }
+                }
+            } else {
+                System.out.print("Message Body-->");
+                for(byte oneByte : bodyByteList) {
+                    System.out.print(oneByte);
+                }
+                System.out.print("<--");
+            }
+        }
+
+        Set<String>paramKeySet = paramMap.keySet();
+        Iterator<String> paramKeyIter = paramKeySet.iterator();
+
+        while(paramKeyIter.hasNext()) {
+            String paramName = paramKeyIter.next();
+            System.out.printf("paramName: %s paramValue: %s\n", paramName, paramMap.get(paramName));
+        }
+
+        System.out.println("End of HTTP Message.");
     }
 }
